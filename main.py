ac438e72d8135790568ef66e4e23f6199e11624f
sumeet kumawat <sumeetjkumawat@gmail.com>
2024-03-18 17:01:02 +0530

    comit day-1 python coding project

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's the name of your pet?\n")
+print("your band name could be " + city_name + " " + pet_name)
